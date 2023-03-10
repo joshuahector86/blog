5cdeda8af407fe0ba1c5a9993590b3705dcfc437
Dimitri Hector <joshuahector86@gmail.com>
2023-03-09 20:32:07 -0800

    adding some hero page content

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -2,16 +2,15 @@ import React from "react";
 import Image from "next/image";
 import photo from "../../assets/jpg1.jpg";
 
-import "./Hero.module.scss";
+import styles from "./Hero.module.scss";
 
 const Hero = () => (
-  <div>
-    <div>
-      <div>Ttitle of the blog</div>
-      <div>Under Title of the blog</div>
+  <div className={styles.hero}>
+    <div className={styles.hero_heading}>
+      <div className={styles.title}>Ttitle of the blog</div>
+      <div className={styles.undertitle}>Under Title of the blog</div>
     </div>
-
-    <Image src={photo} alt="Long Header Photo" width={1000} />
+    <Image src={photo} alt="Long Header Photo" />
   </div>
 );
 
