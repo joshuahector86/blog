d83dc84fb91ca7b859bf8e84ab8aeaa0e7847d57
Dimitri Hector <joshuahector86@gmail.com>
2023-03-09 21:03:24 -0800

    adding big card for posts

--- src/components/Posts/Posts.tsx
+++ src/components/Posts/Posts.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import styles from "./Posts.module.scss";
+import { postBlockMockData } from "@blog/utils/constants";
 
-const Posts = () => {
-  return <div>Posts</div>;
-};
+const Posts = () => (
+  <div className={styles.posts}>
+    <div>Posts Section</div>
+    {/* map function to delegate the information for each post */}
+    <div className={styles.posts_holder}>
+      {postBlockMockData.map((postBlockMockData) => (
+        <div className={styles.post} key={postBlockMockData.title}>
+          <div>{postBlockMockData.title}</div>
+          <div>{postBlockMockData.postPhoto}</div>
+          <div> {postBlockMockData.postInfo}</div>
+        </div>
+      ))}
+    </div>
+  </div>
+);
 
 export default Posts;
--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -6,3 +6,18 @@ export const navlinks = [
   { name: "Videos", link: "/videos/Videos" },
   { name: "The Author", link: "/author/Author" },
 ];
+
+//Mock Data
+
+export const postBlockMockData = [
+  { title: "Title", postInfo: "post info", postPhoto: "post photograph" },
+];
+
+export const footerLinks = [
+  { name: "Features", link: "https://www.google.com" },
+  { name: "Use Cases", link: "https://www.google.com" },
+  { name: "About The Author", link: "https://www.google.com" },
+  { name: "Login/Sign Up", link: "https://www.google.com" },
+  { name: "Email", link: "https://www.google.com" },
+  { name: "Website", link: "https://www.google.com" },
+];
