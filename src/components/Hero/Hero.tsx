28fc97e4d68c473a34441d72626f0e3984b1fb7f
Dimitri Hector <joshuahector86@gmail.com>
2023-03-09 20:14:45 -0800

    finishing the navbar and moving onto the hero section

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Image from "next/image";
+import photo from "../../assets/jpg1.jpg";
 
-const Hero = () => {
-  return <div>Hero</div>;
-};
+import "./Hero.module.scss";
+
+const Hero = () => (
+  <div>
+    <div>
+      <div>Ttitle of the blog</div>
+      <div>Under Title of the blog</div>
+    </div>
+
+    <Image src={photo} alt="Long Header Photo" width={1000} />
+  </div>
+);
 
 export default Hero;
